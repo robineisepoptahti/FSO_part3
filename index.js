df1b6934d0bd376eb839813f0e84270fdc78d722
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2024-12-05 17:13:38 +0200

    added all

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ morgan.token('content', function (req, res) { return JSON.stringify(req.body)})
 let command = ':method :url :status :res[content-length] - :response-time ms :content'
 app.use(morgan(command))
 
-const cors = require('cors')
-
-app.use(cors())
-
 let persons = [
     {
         id: "1",
@@ -32,6 +28,32 @@ let persons = [
         number: "39-23-6423122"
     }
 ]
+
+app.use(express.static('dist'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
